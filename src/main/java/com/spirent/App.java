5c6306e0ffad08a020763c6f1138e865b4b9a6d1
elcaru88 <elcaru88@gmail.com>
2022-04-17 11:40:33 -0300

    First version of file reader

--- src/main/java/com/spirent/App.java
+++ src/main/java/com/spirent/App.java
@@ -2,33 +2,18 @@ package com.spirent;
 
 import com.spirent.service.PhoneNumberService;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    /*
-+7 812 number, +7 (495) number, +7812number, +7815 number, 1-814-number
+public class App {
 
-(812) number,  812number, 812 number, 095-number
+    public static void main(String[] args) throws IOException {
 
-123-4567, 123-45-67, 1234567
- */
-    public static void main( String[] args )
-    {
+
         PhoneNumberService service = new PhoneNumberService();
-        System.out.println(service.normalizePhoneNumberString("+7 812 1234567"));
-        System.out.println(service.normalizePhoneNumberString("+7 (495) 1234567"));
-        System.out.println(service.normalizePhoneNumberString("+78121234567"));
-        System.out.println(service.normalizePhoneNumberString("+7815 1234567"));
-        System.out.println(service.normalizePhoneNumberString("+1-814-1234567"));
-        System.out.println(service.normalizePhoneNumberString("(812) 1234567"));
-        System.out.println(service.normalizePhoneNumberString("8121234567"));
-        System.out.println(service.normalizePhoneNumberString("812 1234567"));
-        System.out.println(service.normalizePhoneNumberString("095-1234567"));
-        System.out.println(service.normalizePhoneNumberString("123-4567"));
-        System.out.println(service.normalizePhoneNumberString("123-45-67"));
-        System.out.println(service.normalizePhoneNumberString("1234567"));
+        service.readPhoneNumbersFromFolder(Path.of("/home/ccaruso/IdeaProjects/spirent/src/main/resources")).forEach(System.out::println);
     }
 }
--- src/main/java/com/spirent/service/PhoneNumberService.java
+++ src/main/java/com/spirent/service/PhoneNumberService.java
@@ -2,6 +2,15 @@ package com.spirent.service;
 
 import com.spirent.model.PhoneNumber;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class PhoneNumberService {
 
 
@@ -14,7 +23,7 @@ public class PhoneNumberService {
             if (Character.isDigit(singleChar)) {
                 if (lastSevenNumbers.length() < 7) {
                     lastSevenNumbers.append(singleChar);
-                } else if ((threeCityNumbers.length() > 0 || Character.isDigit(phoneNumber.charAt(i - 1))) && threeCityNumbers.length() < 3 ){
+                } else if ((threeCityNumbers.length() > 0 || Character.isDigit(phoneNumber.charAt(i - 1))) && threeCityNumbers.length() < 3) {
                     threeCityNumbers.append(singleChar);
                 } else {
                     phoneNumberBuilder.country(String.valueOf(singleChar));
@@ -28,6 +37,29 @@ public class PhoneNumberService {
             phoneNumberBuilder.city("812");
         }
         return phoneNumberBuilder.build().getNormalizedPhoneNumber();
+    }
+
+    public Stream<Path> searchPhoneFilesOnFolder(Path phoneNumberPath) throws IOException {
+        if (!Files.isDirectory(phoneNumberPath)) {
+            throw new IllegalArgumentException("path is not a directory");
+        }
+        return Files.walk(phoneNumberPath).filter(file -> !Files.isDirectory(file));
+    }
+
+    public List<String> readPhoneNumbersFromFolder(Path phoneNumberPath) throws IOException {
+        List<String> result = new ArrayList<>();
+        searchPhoneFilesOnFolder(phoneNumberPath).map(phoneNumberFile -> {
+            try {
+                return readPhoneNumbersFromFile(phoneNumberFile);
+            } catch (IOException e) {
+                return Stream.of("");
+            }
+        }).forEach(phoneNumbers -> result.addAll(phoneNumbers.collect(Collectors.toList())));
+        return result;
+    }
+
+    public Stream<String> readPhoneNumbersFromFile(Path phoneNumberFile) throws IOException {
+        return Files.lines(phoneNumberFile).map(this::normalizePhoneNumberString);
     }
 
 }
